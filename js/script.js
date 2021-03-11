fbad0d21ec725f5b93db76c35ad6d63706e4ccee
web-misha <66106329+web-misha@users.noreply.github.com>
2021-03-11 21:04:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector("body").classList.add("webp");
+  } else {
+    document.querySelector("body").classList.add("no-webp");
+  }
+});
+
+$(document).ready(function () {
+  $(".header_burger").click(function (event) {
+    $(".header_burger,.header_navigation").toggleClass("active");
+  });
+  $(".acordion_item_trigger").click(function () {
+    $(this).toggleClass("acordion_item_trigger_active");
+    $(this).next(".acordion_item_content").slideToggle();
+  });
+});
